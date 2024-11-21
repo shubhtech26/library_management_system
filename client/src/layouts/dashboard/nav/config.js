f5b7fe93e297516fcb22129c0d1c46e8ce6fc299
shubhtech26 <shubhendra.26@gmail.com>
2024-11-20 23:52:16 -0500

    Created Backend with auth

---
+++ client/src/layouts/dashboard/nav/config.js
@@ -0,0 +1,36 @@
+import {FiBookOpen, FiCheckCircle, FiHome, FiList, FiLock, FiUsers} from "react-icons/fi";
+
+const navConfig = [
+  {
+    title: 'Dashboard',
+    path: '/dashboard',
+    icon: <FiHome/>,
+  },
+  {
+    title: 'Books',
+    path: '/books',
+    icon: <FiBookOpen/>,
+  },
+  {
+    title: 'Authors',
+    path: '/authors',
+    icon: <FiUsers/>,
+  },
+  {
+    title: 'Genres',
+    path: '/genres',
+    icon: <FiList/>,
+  },
+  {
+    title: 'Borrowers',
+    path: '/borrowals',
+    icon: <FiCheckCircle/>,
+  },
+  {
+    title: 'Users',
+    path: '/users',
+    icon: <FiLock/>,
+  },
+];
+
+export default navConfig;
